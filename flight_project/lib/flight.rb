66593786b3d8a39595476ea8c56209cb4cdff3de
Joseph Kim <jkjoeeekim@live.com>
2021-05-20 11:23:31 -0700

    Finish flight_project

---
+++ flight_project/lib/flight.rb
@@ -0,0 +1,33 @@
+require_relative './passenger.rb'
+
+class Flight
+    attr_reader :passengers
+
+    def initialize(flight_number, capacity)
+        @flight_number = flight_number
+        @capacity = capacity
+        @passengers = Array.new
+    end
+
+    def full?
+        @passengers.length == @capacity
+    end
+
+    def board_passenger(passenger)
+        if !full?
+            @passengers << passenger if passenger.has_flight?(@flight_number)
+        end
+    end
+
+    def list_passengers
+        @passengers.map { |passenger| passenger.name }
+    end
+
+    def [](index)
+        @passengers[index]
+    end
+
+    def <<(passenger)
+        board_passenger(passenger)
+    end
+end
